57690243c715804318829d253275199924eaacfb
Sebastian Gamez Ariza <juan.gamez1001@gmail.com>
2023-02-17 04:37:14 -0500

    Finalización historial de compras

--- src/profile.js
+++ src/profile.js
@@ -8,10 +8,29 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
+            // Render variables
+            render: {
+                history: false,
+                historyButton: false
+            },
+            // User
             user: JSON.parse(localStorage.getItem('user')),
         }
     },
-    methods: {},
+    methods: {
+        // Toggle the history
+        toggleHistory() {
+            // Check if the user has cards
+            // If the user has cards, toggle the history
+            if (this.user.cards.length > 0) {
+                this.render.history = !this.render.history;
+            }
+            // If the user doesn't have cards, show a message to the user
+            else{
+                swal("No hay historial", "No hay historial de compras", "info");
+            }
+        }
+    },
 });
 
 // Mount the app
